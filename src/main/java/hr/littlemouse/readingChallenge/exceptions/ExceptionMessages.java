10d683d58ea045ea642377d636f9bef2be154154
Davorin Lučić <Akodm44!>
2020-04-24 23:56:16 +0200

    Add custom exceptions description

---
+++ src/main/java/hr/littlemouse/readingChallenge/exceptions/ExceptionMessages.java
@@ -0,0 +1,11 @@
+package hr.littlemouse.readingChallenge.exceptions;
+
+public class ExceptionMessages {
+
+    public static final String USER_DOES_NOT_EXIST = "User does not exist!";
+    public static final String CHALLENGE_DOES_NOT_EXIST = "Challenge does not exist!";
+    public static final String MESSAGE_WRONG_PASSWORD = "Wrong password!";
+    public static final String MESSAGE_WRONG_USER_NAME = "Wrong username!";
+    public static final String PASSWORDS_ARE_NOT_EQUALS = "passwords are not equals! ";
+
+}
